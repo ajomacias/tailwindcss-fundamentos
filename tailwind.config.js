3ed5c8e31433ef728e3fe31127c598ef296c124c
ajo.macias@yavirac.edu.ec <ajo.macias@yavirac.edu.ecuser.nameajomacias>
2021-09-30 17:07:22 -0500

    ..

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: "git",
+  purge: [
+    "./*.html"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
